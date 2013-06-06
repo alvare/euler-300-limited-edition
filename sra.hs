55de25d22a99f47733ab2500e1b7923433fb98f7
alvare <welcometothechango@gmail.com>
2013-06-06 11:12:14 -0300

    18 and 67, fucking monads

--- sra.hs
+++ sra.hs
@@ -116,4 +116,35 @@ scollatz x
     | otherwise = x:[1]
     where p = collatz x
 
+-- 18 & 68 - Max Path
+data18 = [[75],
+          [95, 64],
+          [17, 47, 82],
+          [18, 35, 87, 10],
+          [20, 04, 82, 47, 65],
+          [19, 01, 23, 75, 03, 34],
+          [88, 02, 77, 73, 07, 63, 67],
+          [99, 65, 04, 28, 06, 16, 70, 92],
+          [41, 41, 26, 56, 83, 40, 80, 70, 33],
+          [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+          [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+          [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+          [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+          [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+          [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23]]
+
+readInt s = read s :: Int
+readInts s = map readInt (words s)
+parse s = map readInts (lines s)
+
+sumPaths :: (Num a, Ord a) => [[a]] -> [a]
+sumPaths [] = [0]
+sumPaths (x:[]) = x
+sumPaths (x:y:xs) = sumPaths ((zipWith max left right) : xs)
+    where left = (zipWith (+) x y) ++ [0]
+          right = 0 : (zipWith (+) x ys)
+          ys = tail y
+
+main = readFile "fucking-triangle.txt" >>= putStrLn . (show.maximum.sumPaths.parse)
+
 --main = time $ putStrLn $ show $ sum $ primesBelow 2000000
