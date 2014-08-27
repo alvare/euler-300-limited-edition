f945897cd984c6e349c549c335c26273982bfada
ealvarez <ealvarez@msa.com.ar>
2014-08-27 16:09:49 -0300

    100 numbers, 25Megs, lol

--- haskell/27-bis-quadratic-primes.hs
+++ haskell/27-bis-quadratic-primes.hs
@@ -4,27 +4,30 @@ import Data.List
 import Data.Numbers.Primes
 import System.Random
 
-type NInt = Integer
+type NInt = Int
 
 quadratic :: (Num a) => a -> a -> a -> a
 quadratic a b n = n^2 + a*n + b
 
-semiPrime x s = x `S.member` s || isPrime x
+semiPrime x s = (abs x) `S.member` s || (isPrime . fromIntegral . abs $ x)
 
 consPrim :: NInt -> NInt -> S.Set NInt -> (S.Set NInt, NInt)
 consPrim a b primes = loop a b 0 primes
-    where loop x y n s = if semiPrime new s then loop x y (succ n) (S.insert new s)
+    where loop x y n s = if semiPrime new s then loop x y (succ n) ((abs new) `S.insert` s)
                          else (s, n)
               where new = quadratic x y n
 
-quadLen :: S.Set NInt -> [(NInt, NInt)] -> [NInt] -> [NInt]
-quadLen primes [] list = list
+quadLen :: S.Set NInt -> [(NInt, NInt)] -> [NInt] -> (S.Set NInt, [NInt])
+quadLen primes [] list = (primes, list)
 quadLen primes ((a, b):xs) list = if semiPrime b primes then quadLen primes' xs ((n:list) `seq` (n:list))
                                   else quadLen primes xs list
-                              where primes' = S.unions [primes'', primes, S.singleton b]
+                              where primes' = S.union primes'' ((abs b) `S.insert` primes)
                                     (primes'', n) = consPrim a b primes
 
-n = 1000 :: NInt
+n = 100 :: NInt
 pairs = [(a, b) | a <- [-n..n], b <- [-n..n]]
 
-main = putStrLn . show . maximumBy (comparing snd) . zip pairs . quadLen S.empty pairs $ []
+main = do
+    putStrLn . show . maximumBy (comparing snd) . zip pairs $ lens
+    putStrLn . show $ set
+    where (set, lens) = quadLen S.empty pairs []
